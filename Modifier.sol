2cddcabd0bd7f89c195deeda99f00deb849800f9
shehin fn <shehinfn@shehins-MacBook-Air.local>
2021-02-27 16:26:47 +0530

    added modifier to allow only the contract owner to run functions

---
+++ Modifier.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.8.0;
+
+contract MyContract {
+    mapping(uint=>Person) public peopleMap;
+    uint256  public peopleCount;
+    struct Person{
+        uint id;
+        string firstname;
+        string lastname;
+    }
+
+
+     function addPersonMap(string memory firstname , string memory lastname) public{
+        peopleMap[peopleCount] = Person(peopleCount,firstname,lastname);
+         incrementCounter();
+    }
+
+    function incrementCounter(){
+        peopleCount++;
+    }
+}
